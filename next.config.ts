2d37ad95c0d7822f1ebda716f4be74266e1e3d92
Reza Aditya <reza.aditya@pelni.co.id>
2025-04-15 10:52:35 +0700

    skip eslint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ⛔ ESLint akan di-skip saat `next build`
+  },
 };
 
 export default nextConfig;
